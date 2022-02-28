8c91036ff900111c0c045a20371af54fa785838f
Victor Watanabe <vmwatanabe@gmail.com>
2022-02-28 13:48:14 -0300

    feature: added format

--- src/components/stock-compare/stock-compare.helpers.js
+++ src/components/stock-compare/stock-compare.helpers.js
@@ -1,16 +1,33 @@
+import {
+  formatMoney,
+  formatNumber,
+  formatPercentage,
+} from "../stock-list/stock-list.helpers";
+
 const rows = [
   { factor: "lower", prop: "magicRanking", name: "Magic ranking" },
   { prop: "setor", name: "Setor" },
   { prop: "industria", name: "Indústria" },
-  { factor: "lower", prop: "pByL", name: "P/L" },
-  { factor: "higher", prop: "dividendYield", name: "Div.Yield" },
-  { factor: "higher", prop: "roic", name: "ROIC" },
-  { factor: "higher", prop: "roe", name: "ROE" },
-  { factor: "higher", prop: "patrimonioLiquido", name: "Patri. Líq" },
+  { factor: "lower", prop: "pByL", name: "P/L", formatter: formatNumber },
+  {
+    factor: "higher",
+    prop: "dividendYield",
+    name: "Div.Yield",
+    formatter: formatPercentage,
+  },
+  { factor: "higher", prop: "roic", name: "ROIC", formatter: formatPercentage },
+  { factor: "higher", prop: "roe", name: "ROE", formatter: formatPercentage },
+  {
+    factor: "higher",
+    prop: "patrimonioLiquido",
+    name: "Patri. Líq",
+    formatter: formatMoney,
+  },
   {
     factor: "lower",
     prop: "dividaBrutaByPatrimonio",
     name: "Dív.Brut/ Patrim.",
+    formatter: formatNumber,
   },
 ];
 
@@ -23,8 +40,15 @@ export const getColumns = (stocks) => {
     ...stocks.map((stock) => ({
       id: stock.papel,
       name: stock.papel,
+      formatter: (value, row) => {
+        const rowFormatter = rows.find(
+          (elem) => elem.name === row.cells[0].data
+        ).formatter;
+
+        return rowFormatter?.(value) ?? value;
+      },
       attributes: (cell, row) => {
-        if (!(cell && row)) return;
+        if (!(cell !== null && row !== null)) return;
 
         const rowFactor = rows.find(
           (elem) => elem.name === row.cells[0].data
